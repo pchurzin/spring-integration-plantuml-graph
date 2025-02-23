2c20775bd927e889994fc0749fb190f45423e106
Pavel Churzin <pavel@pchurzin.ru>
2025-02-23 13:26:25 +0300

    create maven central bundle

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,17 @@ plugins {
     `java-library`
     `jvm-test-suite`
     alias(libs.plugins.kotlin.jvm)
+    `maven-publish`
+    signing
 }
 
 kotlin {
     jvmToolchain(17)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
 }
 
 dependencies {
@@ -27,4 +34,54 @@ testing {
             useJUnitJupiter()
         }
     }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "StagingDeploy"
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
+
+        mavenLocal()
+    }
+
+    publications {
+        create<MavenPublication>("release") {
+            from(components["java"])
+            pom {
+                name = "Spring Integration PlantUML Graph"
+                description = "A tiny library for generating PlantUML diagrams from Spring Integration Graph"
+                url = "https://github.com/pchurzin/spring-integration-plantuml-graph"
+                licenses {
+                    license {
+                        name = "MIT License"
+                        url = "https://opensource.org/license/mit"
+                    }
+                }
+                developers {
+                    developer {
+                        name = "Pavel Churzin"
+                        email = "pchurzin@yandex.ru"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/pchurzin/spring-integration-plantuml-graph.git"
+                    developerConnection = "scm:git:ssh://github.com/pchurzin/spring-integration-plantuml-graph.git"
+                    url = "https://github.com/pchurzin/spring-integration-plantuml-graph"
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["release"])
+    useGpgCmd()
+}
+
+val createMavenCentralBundle = tasks.register<Zip>("createMavenCentralBundle") {
+    dependsOn(tasks.named("publishReleasePublicationToStagingDeployRepository"))
+    from(layout.buildDirectory.dir("staging-deploy"))
+    archiveFileName.set("maven-central-bundle.zip")
 }
